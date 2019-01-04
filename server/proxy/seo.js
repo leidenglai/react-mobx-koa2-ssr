a6b96fb7d54a40f8dfa131a70b9e1690d9eb3732
leidenglai <819441338@qq.com>
2019-01-04 14:22:50 +0800

    将mongodb 替换为从json文件取数据

--- server/proxy/seo.js
+++ server/proxy/seo.js
@@ -1,4 +1,6 @@
-import { Seo } from '../models'
+// import { Seo } from '../models'
+import seoJson from '../data/seo.json'
+import _ from 'lodash'
 
 /**
  * 获取用户信息
@@ -7,7 +9,9 @@ import { Seo } from '../models'
  * @return {Object} seoInfo 页面的seo信息
  */
 export async function getSeoInfo(url) {
-  const seoInfo = await Seo.findOne({ url }, { _id: 0, __v: 0 }, { lean: true }).exec()
+  // const seoInfo = await Seo.findOne({ url }, { _id: 0, __v: 0 }, { lean: true }).exec()
+  // 暂用json代替数据库
+  const seoInfo = _.find(seoJson, { url })
 
   return seoInfo
 }
--- server/proxy/user.js
+++ server/proxy/user.js
@@ -1,10 +1,11 @@
-import { User } from '../models'
+// import { User } from '../models'
+import user from '../data/user.json'
 
 /**
  * 获取用户信息
  */
 export async function getUserData() {
-  const user = await User.findOne({ uid: 1 }, { uid: 0, _id: 0, __v: 0 }, { lean: true }).exec()
+  // const user = await User.findOne({ uid: 1 }, { uid: 0, _id: 0, __v: 0 }, { lean: true }).exec()
 
   return user
 }
